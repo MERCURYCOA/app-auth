740f7099547edf7d537a11572cfe4c520226b95c
曹胜楠 <caoshengnan@caoshengnandeMacBook-Air.local>
2018-11-08 11:37:05 -0500

    user-auth

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  // database: 'mongodb://mercury:Aa19911022@ds151383.mlab.com:51383/pprpapp',   //prod
+  database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'yoursecret'
+}
